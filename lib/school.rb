615fa4f3cf35166b497914030ead56d713141c74
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-05-31 10:42:03 -0700

    commit tree e64f743febf79524d51d02c0e7a864de621521f7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(full_name, grade)
+        roster[grade] ||= []
+        roster[grade] << full_name
+    end
+
+    def grade(retrieve_student)
+        roster[retrieve_student]
+    end
+
+    def sort
+        #idea shared by malcom from group-work
+        arranged_roster = {}
+        roster.each do |grade, full_name|
+            arranged_roster[grade] = full_name.sort
+        end
+        arranged_roster
+      end
+    end
